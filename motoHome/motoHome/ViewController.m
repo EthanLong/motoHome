bbcd42fe4e30eef5c74c871c7263d9b601e2debd
EthanLong <roger_wen@live.cn>
2016-04-23 18:06:59 +0800

    ethan
    mm

--- motoHome/motoHome/ViewController.m
+++ motoHome/motoHome/ViewController.m
@@ -16,7 +16,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib. test github
+    AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+    
+    session.securityPolicy.allowInvalidCertificates = YES;
+    [session GET:@"http://www.weather.com.cn/data/sk/101010100.html" parameters:nil progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        
+        NSLog(@"成功");
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        NSLog(@"失败 %@",error);
+    }];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
